bf4d238c90cf9b9258e2605fae49acf84665280f
DESKTOP-666L4IT\kimyejin <yejin2964@gmail.com>
2023-03-28 20:51:29 +0900

    Setting

---
+++ MediaProject-2/src/main/java/com/yejin/mediaproject/MediaProject2Application.java
@@ -0,0 +1,13 @@
+package com.yejin.mediaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaProject2Application.class, args);
+	}
+
+}
---
+++ MediaProject-2/src/test/java/com/yejin/mediaproject/MediaProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yejin.mediaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
