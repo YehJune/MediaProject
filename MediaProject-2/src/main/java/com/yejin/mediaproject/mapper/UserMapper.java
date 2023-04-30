1245aaf75ec9209156d107e90f3acbfb93974d76
DESKTOP-666L4IT\kimyejin <yejin2964@gmail.com>
2023-04-30 23:01:57 +0900

    마이바티스 테스트케이스 작성

---
+++ MediaProject-2/src/main/java/com/yejin/mediaproject/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.yejin.mediaproject.mapper;
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import com.yejin.mediaproject.domain.User;
+@Mapper
+public interface UserMapper {
+	@Insert("INSERT INTO USERS(ID, USERNAME,PASSWORD, EMAIL)VALUES((SELECT NVL(MAX(ID),0) + 1 FROM USERS), #{username}, #{password}, #{email})")
+	public void insertUser(User user);
+	
+	@Update("UPDATE USERS PASSWORD = #{password}, EMAIL = #{email} WHERE ID = #{id}")
+	public void updateUser(User user);
+	
+	@Delete("DELETE USERS WHERE ID = #{id}")
+	public void deleteUser(User user);
+	
+	@Select("SELECT * FROM USERS WHERE USERNAME = #{username}")
+	public void getUser(User user);
+	
+	@Select("SELECT * FROM USERES ORDER BY USERNAME DESC")
+	public List<User> getUserList();
+}
